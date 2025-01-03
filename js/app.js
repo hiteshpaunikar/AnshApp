634241275f9abbfef46db433252b446f2c8b78fb
hiteshpaunikar <h.paunikar@gmail.com>
2025-01-03 11:06:37 +0530

    ok
    ok

---
+++ js/app.js
@@ -0,0 +1,52 @@
+    if ("serviceWorker" in navigator) {
+		window.addEventListener('load',() => {
+    	    navigator.serviceWorker
+    	      .register("./serviceWorker.js")
+    	      .then(res => {
+				console.log(res);
+				
+			  })
+    	      .catch(err => console.log( err));
+			});
+    	    
+    	    /*setTimeout(() => {
+    	      
+    	    	console.log("Timed out..");
+    	      }, 3000);*/
+    	  
+			  
+			  if(window.location.pathname === '/AnshApp/About.html') {
+				navigator.serviceWorker.ready.then((registration) => {
+					registration.active.postMessage({action:'getCacheName'});
+				});
+
+				navigator.serviceWorker.addEventListener('message',(event)=> {
+					if(event.data.action ==='setCacheName') {
+						document.getElementById('cache-name').textContent=event.data.cacheName;
+					}
+				})
+
+			  }
+
+//    	  navigator.serviceWorker.register("./serviceWorker.js");
+    	    /*window.addEventListener('beforeinstallprompt', event => {
+    	        event.preventDefault();
+    	        var panelInstall = document.getElementById('panelInstall');
+    	    //    panelInstall.style.display = "block";
+    	        
+    	        // var btnInstall=document.getElementById('btnInstall');
+    	        // btnInstall.addEventListener('click', () => {
+    	        //     event.prompt();
+    	        //     panelInstall.style.display = "none";
+    	        //   });
+    	        
+
+    	        console.log('before install prompt');
+    	    	  
+    	      }); */
+
+
+			 
+			 // if we use Localstorage instead of session storage, it will save permanently and will not open , even though, browser close and open
+			
+    	}
